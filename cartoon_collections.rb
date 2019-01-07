87c7512a014114d37f0222c97e0a7c94803ffe61
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-07 19:59:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.find do |item|
-    item == cheese_types.each {|cheese| cheese}
+    item.cheese_types?
   end
 end
